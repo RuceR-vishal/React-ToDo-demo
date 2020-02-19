73bda12fa1d8c8b24b993875c2619d84fdbad3fd
Vishal V. Chaudhary <vishal.chaudhary@gatewaygroup.com>
2020-02-19 12:02:33 +0530

    Updates

---
+++ precache-manifest.5e8bbee191a6206e9825fe26cd8f5dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456463d1982523d533e0712aadd17588",
+    "url": "/React-ToDo-demo/index.html"
+  },
+  {
+    "revision": "69bbed3ec2ca72af5b36",
+    "url": "/React-ToDo-demo/static/css/main.f1686178.chunk.css"
+  },
+  {
+    "revision": "31318bf7a2264d6be307",
+    "url": "/React-ToDo-demo/static/js/2.f6ee6750.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-ToDo-demo/static/js/2.f6ee6750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bbed3ec2ca72af5b36",
+    "url": "/React-ToDo-demo/static/js/main.7e34d417.chunk.js"
+  },
+  {
+    "revision": "e982de6b414b39f26389",
+    "url": "/React-ToDo-demo/static/js/runtime-main.a6dfbdbe.js"
+  }
+]);
